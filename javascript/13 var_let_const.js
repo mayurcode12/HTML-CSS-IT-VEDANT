30a734eaad0564c759bd210742f747f408327e6d
mayurcode12 <mayurchaudhari41412@gmail.com>
2025-11-12 20:58:58 +0530

    fc

---
+++ javascript/13 var_let_const.js
@@ -0,0 +1,16 @@
+if (true) {
+  var i = 20;
+  console.log("inside function i", i);
+}
+console.log("Outside function i", i);
+
+var a = 1;
+let b = 2;
+const c = 3;
+
+a = 11;
+b = 22;
+// c = 33;
+console.log(a);
+console.log(b);
+console.log(c);
